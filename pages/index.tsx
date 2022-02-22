f5f05a3acb9a4f9949d1613a5a44a48c1f2a726f
João Reis <joaoreisjpk@gmail.com>
2022-02-22 20:17:31 -0300

    moving the data fetching from index to calculadora/prices pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,10 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
-import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   const { push } = useRouter();
-  useEffect(() => {
-    const api = async () => {
-      const response = await fetch('http://localhost:3000/api/prices');
-      console.log(await response.json());
-    };
-    api();
-  }, []);
-
   return (
     <div>
       <Head>
