5bdc4f08e44f8ce7cd3d7000c6d71fdace16264f
João Reis <joaoreisjpk@gmail.com>
2022-06-06 12:09:48 -0300

    initial commit

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   projectId: 'aaehz6',
+  env: {
+    CYPRESS_RECORD_KEY: '2b4858de-9c71-4a91-8591-e1798025e50e',
+  },
   viewportHeight: 920,
   viewportWidth: 1420,
   e2e: {
